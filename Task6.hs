7abe61a538bc0076fbe0dfecb9823650ad371e47
scriptomax <likemail>
2020-01-17 22:06:03 +0100

    Request for assessment

--- Task6.hs
+++ Task6.hs
@@ -15,12 +15,15 @@ type Parser a = Parsec String () a
 digit :: Parser Char
 digit = oneOf ['0'..'9']
 
+-- try obtaining some floating point (primarily) or integer numbers from string input 
 number :: Parser Double
 number = try calcDouble <|> calcInt  
 
+-- integer handler 
 calcInt :: Parser Double 
 calcInt = read <$> many1 digit
 
+-- floating point handler 
 calcDouble :: Parser Double 
 calcDouble = do
     integ <- many1 digit
@@ -28,11 +31,9 @@ calcDouble = do
     decim <- many1 digit
     pure $ read $ integ <> "." <> decim	 
  
-numberFact :: Parser Integer  -- particularly to let parser consume integers strictly as `factorial` inputs     
+-- defined particularly to let parser consume integers as a `factorial` normal sort of arguments 
+numberFact :: Parser Integer         
 numberFact = read <$> many1 digit
-
-fp_char :: Parser String 
-fp_char = many1 digit
 
 applyMany :: a -> [a -> a] -> a
 applyMany x [] = x
@@ -61,7 +62,7 @@ minus = do
 multiplication :: Parser Double
 multiplication = do
 	spaces
-	lhv <- atom <|> fact' <|> negation'
+	lhv <- try fact' <|> atom <|> negation'
 	spaces
 	t <- many tail
 	return $ applyMany lhv t
@@ -69,7 +70,7 @@ multiplication = do
 				do
 					f <- star <|> div_
 					spaces
-					rhv <- atom <|> fact' <|> negation'
+					rhv <- try fact' <|> atom <|> negation'
 					spaces
 					return (`f` rhv)
  	
@@ -80,15 +81,15 @@ atom = number <|> do
 	char ')'
 	return res
 
--- factorial  (`!` set in prior to a number because test function calls have been skipped when `!` followed that number. 
--- A bit confusing, so it's a kind of whatever to be working) 	
+-- factorial argument picker  
 fact' :: Parser Double
 fact' = do
     spaces
-    char '!'
     rhv <- numberFact
+    char '!'
     return $ factorial rhv
 
+-- factorial computation comes in action once picker succeeds fetching valid argument  
 factorial :: Integer -> Double
 factorial n
 	| n < 0 = error "No factorial exists for negative inputs" 
@@ -107,11 +108,11 @@ negation' = do
 	spaces
 	return $ negate rhv	
 
--- core function to (re)run parser tests calling `parseTest addition {some test string input}`	
+-- core function to (re)run parser tests (command to call parser: `parseTest addition "{some test string input}"`)	
 addition :: Parser Double
 addition = do
 	spaces
-	lhv <- multiplication <|> fact' <|> negation'
+	lhv <- multiplication <|> negation'
 	spaces
 	t <- many tail
 	return $ applyMany lhv t
@@ -119,9 +120,6 @@ addition = do
 				do
 					f <- plus <|> minus
 					spaces
-					rhv <- multiplication <|> fact' <|> negation'
+					rhv <- try multiplication <|> negation'
 					spaces
 					return (`f` rhv)
-					
-
-	
